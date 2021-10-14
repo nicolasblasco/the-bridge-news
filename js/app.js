8320a6a10e5e7d1ca62396db3134275c7d5871a8
nicolasblasco <nicoblasco96@gmail.com>
2021-10-14 19:57:20 -0300

    Added phone number validation code

--- js/app.js
+++ js/app.js
@@ -13,3 +13,51 @@ function fullNameBlur(e) {
 function fullNameFocus(e) {
     fullNameVal.style.opacity = '0';
 }
+
+// Email validation
+
+
+
+// Password validation
+
+
+
+// Confirm password validation
+
+
+
+// Age validation
+
+
+
+// Phone number validation
+
+var phoneNumber = document.getElementById('phone-number-input');
+var phoneNumberVal = document.getElementById('phone-number-val');
+phoneNumber.addEventListener('blur', phoneNumberBlur);
+phoneNumber.addEventListener('focus', phoneNumberFocus);
+function phoneNumberBlur(e) {
+    var x = phoneNumber.value;
+    if(x.length < 7 || isNaN(x)) {
+        phoneNumberVal.style.opacity = '1';
+    } 
+}
+function phoneNumberFocus(e) {
+    phoneNumberVal.style.opacity = '0';
+}
+
+// Address validation
+
+
+
+//City validation
+
+
+
+// Postal code validation 
+
+
+
+// ID validation
+
+
